14d69a0484132120e3af6131deb694fc28846525
Gerry Bong <ggbong734@gmail.com>
2021-11-03 21:58:10 -0700

    codewars duplicateEncoder

---
+++ codewars_duplicateEncoder_6kyu.js
@@ -0,0 +1,8 @@
+// convert word to lower case
+// replace each char
+// check if the first index that the char appears is the same as the last index that it appears
+// if true, char appears once so replace with (, otherwise char appears more than once and replace with )
+function duplicateEncode(word){
+  word = word.toLowerCase()
+  return word.replace(/./g, c => word.indexOf(c) == word.lastIndexOf(c) ? '(' : ')');
+}
