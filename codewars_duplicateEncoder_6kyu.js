337dd65c113a7052dfb4a6b3c1820d9069560eda
Gerry Bong <ggbong734@gmail.com>
2021-11-04 22:29:13 -0700

    minor comment change

--- codewars_duplicateEncoder_6kyu.js
+++ codewars_duplicateEncoder_6kyu.js
@@ -1,5 +1,5 @@
 // convert word to lower case
-// replace each char
+// replace each char using the regex /./g for each character
 // check if the first index that the char appears is the same as the last index that it appears
 // if true, char appears once so replace with (, otherwise char appears more than once and replace with )
 function duplicateEncode(word){
