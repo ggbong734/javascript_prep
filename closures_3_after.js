9f4409ee83340f7007b8b8122b454c08a6d0f368
Gerry Bong <ggbong734@gmail.com>
2021-11-10 22:26:11 -0800

    replaced after closures

--- closures_3_after.js
+++ closures_3_after.js
@@ -1,27 +1,23 @@
-// define after as a function with two arguments, a number and a callback function
-// initialize a variable called counter and set it to zero
-// define a function monitoredCb with an argument that is variable
-// increment counter when invoking this function
-// then check if counter is equal or more than the num
-// if so run the callback function with arg as the argument. otherwise do nothing.
-// then in the after function context, return monitoredCb function as the output to after.
+// define a function after with two arguments, num and callback
+// returns an anonymous function which can take any argument, so we pass in with rest parameter
+// declare a variable in the local execution context of the function, call it counter = 0
+// this helps us keep track of number of times we have run the function
+// in the inner function, we increment counter
+// then we run an if statement to check if the counter is more or equal to num, if so, we return the callback with the argument passed in
+
 
 function after(num, callback) {
-	let counter = 0;
-  function monitoredCb(...arg) {
-    counter++;
-    if (counter >= num) {
-      return callback(...arg);
-    }
+  let counter = 0;
+  return function (...arg) {
+    counter += 1;
+    console.log(counter);
+    if(counter >= num) {return callback(...arg);}
   }
-  return monitoredCb;
 }
 
+const callb = function (string) {return ('hello' + string);};
+const myFunc = after(3, callb);
 
-const called = function(string) { return('hello ' + string); };
-const afterCalled = after(3, called);
-
-// UNCOMMENT THESE LINES TO TEST YOUR WORK
-console.log(afterCalled('world')); // -> undefined is printed
-console.log(afterCalled('world')); // -> undefined is printed
-console.log(afterCalled('world')); // -> 'hello world' is printed
+console.log(myFunc('world'));
+console.log(myFunc('world'));
+console.log(myFunc('world'));
