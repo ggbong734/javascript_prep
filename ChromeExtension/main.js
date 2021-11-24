27e895ec3b5caee0be499e087117471f85516fce
Gerry Bong <ggbong734@gmail.com>
2021-11-23 22:12:24 -0800

    complete part 4 of chrome extension

--- ChromeExtension/main.js
+++ ChromeExtension/main.js
@@ -1,2 +1,17 @@
-const contents = document.getElementById("contents");
-contents.parentNode.removeChild(contents);
+// Vanilla JS
+// const contents = document.getElementById("contents");
+// let contentsParent = contents.parent();
+// contents.parentNode.removeChild(contents);
+
+// jQuery version
+let contents = $('#contents');
+let contentsParent = contents.parent();
+contents.remove();
+
+let scrollBar = $('#chips');
+scrollBar.remove();
+
+contentsParent.prepend('<img id="motivation" src="https://iamfearlesssoul.com/wp-content/uploads/2016/10/success-video-1-759x500.jpg"/>');
+contentsParent.prepend("<span class='beautText'></b>Get back to building your dreams!<b></span>");
+
+message.addClass('beautText');
