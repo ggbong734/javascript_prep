fff5dc9772dba4505211067893ed7e9e0dc24576
Gerry Bong <ggbong734@gmail.com>
2021-12-17 23:44:33 -0800

    minor change 3

--- callbacks_1_pluralize.js
+++ callbacks_1_pluralize.js
@@ -1,39 +1,39 @@
-// ADD CODE HERE
+// ADD CODE HERE..
 
 function pluralize(arr) {
-    let newArr = []
-    for (let i=0; i < arr.length ; i++) {
-      newArr[i] = arr[i] + "s"
-    }
-    return newArr
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    newArr[i] = arr[i] + "s"
   }
-  // Uncomment these to check your work!
-  const animals = ["dog", "cat", "tree frog"];
-  console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
+  return newArr
+}
+// Uncomment these to check your work!
+const animals = ["dog", "cat", "tree frog"];
+console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
 
 
-  // define function takes in an array of strings
-  // it will loop through each string in the array and concat an s to the string
+// define function takes in an array of strings
+// it will loop through each string in the array and concat an s to the string
 
-  let strings = ['dog', 'cat', 'man dog']
+let strings = ['dog', 'cat', 'man dog']
 
 
-  function pluralize(arr) {
-    let newArr = [];
-    arr.forEach((string, idx, array) => newArr[idx] = string.concat('s'));
-    return newArr
+function pluralize(arr) {
+  let newArr = [];
+  arr.forEach((string, idx, array) => newArr[idx] = string.concat('s'));
+  return newArr
+}
+
+console.log(pluralize(strings));
+
+let newArr = ['human', 'tiger', 'lion']
+
+function pluralizer(arr) {
+  let outputArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    outputArr[i] = arr[i] + "s";
   }
+  return outputArr;
+}
 
-  console.log(pluralize(strings));
-
-  let newArr = ['human', 'tiger', 'lion']
-
-  function pluralizer(arr) {
-      let outputArr = [];
-    for (let i=0; i<arr.length; i++) {
-        outputArr[i] = arr[i] + "s";
-      }
-    return outputArr;
-  }
-
-  console.log(pluralizer(newArr));
+console.log(pluralizer(newArr));
